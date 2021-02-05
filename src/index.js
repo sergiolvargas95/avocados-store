5c445851b7b6e75bc16df3f0fc9dd55af2c23f30
sergiolvargas95 <65960993+sergiolvargas95@users.noreply.github.com>
2021-02-04 21:35:12 -0500

    desarrollo de una tienda bajando los datos de una API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+/*RESUMEN: NOS CONECTAMOS A UNA API QUE ES UN PUENTE CON LA INFORMACION
+DE UN SERVIDOR Y ESE SERVIDOR NOS DEVUELVE ESA INFORMACION, Y UTILIZAMOS
+UN CICLO POR CADA UNO DE LOS ELEMENTOS QUE NOS DEVUELVE ESE SERVIDOR
+CREAMOS NODOS Y SE LOS AGREGAMOS AL FINAL A NUESTRO HTML*/
 
-console.log('Happy hacking :)')
+const baseUrl = "https://platzi-avo.vercel.app";
+
+const appNode = document.querySelector('#app')
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(price);
+
+    return newPrice;
+}
+window
+    .fetch(`${baseUrl}/api/avo`)
+    .then(respuesta => respuesta.json())
+    .then(responseJson => {
+        const todosLosItems = [];
+        responseJson.data.forEach((item) => {
+            const imagen = document.createElement('img');
+            imagen.src =  `${baseUrl}${item.image}`
+            imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
+
+            const title = document.createElement('h2');
+            title.textContent = item.name;
+            title.className = 'text-lg'
+
+            const price = document.createElement('div');
+            price.textContent = formatPrice(item.price);
+            price.className = 'text-gray-600'
+
+            const priceAndTitle = document.createElement('div')
+            priceAndTitle.className = 'text-center md:text-left'
+            priceAndTitle.append(title, price)
+
+            //Wrap Img and priceAndTitle
+            const card = document.createElement('div')
+            card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300"
+
+            card.append(imagen, priceAndTitle)
+
+            todosLosItems.push(card);
+        });
+
+        appNode.append(...todosLosItems)
+    });
